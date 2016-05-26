386de8ce52404fe605ad4e62ac72ef5ba975e4e4
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-05-27 00:11:51 +0200

    Updated dict slightly

--- PAWServer/src/Main.java
+++ PAWServer/src/Main.java
@@ -6,6 +6,7 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 /**
  * Created by Tom Dobbelaere on 26/05/2016.
@@ -14,33 +15,11 @@ public class Main
 {
     public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException, URISyntaxException, InterruptedException
     {
-        /*Clip clip = AudioSystem.getClip();
-        AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("C:\\test\\a.wav"));
-
-        byte[] ayy = new byte[inputStream.available() * 2];
-        int n = inputStream.read(ayy);
-        inputStream = AudioSystem.getAudioInputStream(new File("C:\\test\\b.wav"));
-        inputStream.read(ayy, n, inputStream.available());
-
-        clip.open(inputStream.getFormat(), ayy, 0, ayy.length);
-        clip.start();
-
-        while(clip.getFramePosition() != clip.getFrameLength())
-        {
-        }
-
-        System.out.println(inputStream.getFormat().getSampleRate());
-        System.out.println(inputStream.getFormat().getSampleSizeInBits());
-        System.out.println(inputStream.getFormat().getChannels());
-        System.out.println(inputStream.getFormat().isBigEndian());
-        System.out.println(inputStream.getFormat().properties());*/
-
-
         PhonemeFileReader reader = new PhonemeFileReader("/cmudict-test.dict");
         SpeechBank speechBank = new SpeechBank("digaly");
         SpeechSynthesizer synthesizer = new SpeechSynthesizer(speechBank, reader.getPhonemeBank());
 
-        synthesizer.speak("well behold");
+        /*synthesizer.speak("well behold");
         Thread.sleep(500);
         synthesizer.speak("this is my artificial voice");
         Thread.sleep(500);
@@ -56,7 +35,16 @@ public class Main
         Thread.sleep(500);
         synthesizer.speak("thank you for listening");
         Thread.sleep(500);
-        synthesizer.speak("digaly out");
+        synthesizer.speak("digaly out");*/
+
+        System.out.print("Enter your sentence: ");
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String s1 = sc.next();
+            synthesizer.speak(s1);
+        }
+
 
         //synthesizer.speakWord("great");
     }
